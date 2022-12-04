9d7939f80030d2973d69bc7d15d2507c610a13ae
sanchez1987-1 <sanchez1987-1@mail.ru>
2022-12-04 22:16:15 +0500

    item click

--- app/src/main/java/ru/myhousingservice/myservices/MainActivity.kt
+++ app/src/main/java/ru/myhousingservice/myservices/MainActivity.kt
@@ -1,18 +1,29 @@
 package ru.myhousingservice.myservices
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import ru.myhousingservice.myservices.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding            : ActivityMainBinding
+    private lateinit var resultLauncher : ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                // There are no request codes
+                val data: Intent? = result.data
+                //doSomeOperations()
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -35,7 +46,7 @@ class MainActivity : AppCompatActivity() {
             }
             R.id.tickets -> {
                 val intent = Intent(this, TicketsListActivity::class.java)
-                startActivity(intent)
+                resultLauncher.launch(intent)
             }
         }
         return super.onOptionsItemSelected(item)
